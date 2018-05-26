8fa3fe41ffb5c0fb07e7df6854e8857b0ef90ccf
chenxi.zhou <chenxi.zhou@IMB12-010514-LT.ad.imb.uq.edu.au>
2018-05-27 02:50:40 +1000

    update

--- src/cz1/tenx/tools/HaplotypeSimulator.java
+++ src/cz1/tenx/tools/HaplotypeSimulator.java
@@ -78,50 +78,67 @@ public class HaplotypeSimulator extends Executor {
 		List<Sequence> hapseqs = Sequence.parseFastaFileAsList(hap_in);
 		if(hapseqs.size()>1) 
 			throw new RuntimeException("Multiple haplotype sequence not supported!!!");
-		String hapseq = hapseqs.get(0).seq_str();
+		final String seq_sn = hapseqs.get(0).seq_sn();
+		final String hapseq = hapseqs.get(0).seq_str();
 		final StringBuilder[] haps = new StringBuilder[hap_copy];
 		// we initiate a StringBuilder of size slightly greater than the reference haplotype
 		for(int i=0; i<hap_copy; i++) haps[i] = new StringBuilder((int)(1.1*hapseq.length()));
 		try {
 			BufferedReader br_vcf = Utils.getBufferedReader(vcf_in);
-			String line, allele;
+			String line;
 			String[] s, s2;
-			int pos, refz, offset = 0;
+			int pos, pos_prev = -1, refz, offset = 0, h;
+			
+			BufferedWriter bw_hap = Utils.getBufferedWriter(out_f+".hap");
+			bw_hap.write("#CHROM\tPOS\tID\tREF\tALT");
+			for(int i=0; i<hap_copy; i++) bw_hap.write("\t"+i);
+			bw_hap.write("\n");
 			
 			while( (line=br_vcf.readLine())!=null ) {
 				if(line.startsWith("#")) continue;
 				s = line.split("\\s+");
 				pos = Integer.parseInt(s[1])-1;
+				if(pos<=pos_prev) continue;
 				s2 = s[4].split(",");
 				final String[] alleles = new String[1+s2.length];
 				alleles[0] = s[3];
 				System.arraycopy(s2, 0, alleles, 1, s2.length);
 				refz = alleles[0].length();
 				
-				if(!hapseq.substring(pos,pos+refz).equals(alleles[0]))
-					throw new RuntimeException("!!!");
+				if(!hapseq.substring(pos,pos+refz).equals(alleles[0])) {
+					alleles[0] = Sequence.revCompSeq(alleles[0]);
+					if(!hapseq.substring(pos,pos+refz).equals(alleles[0])) 
+						// throw new RuntimeException(pos+","+refz+","+alleles[0]+"!!!");
+						continue;
+					for(int i=1; i<alleles.length; i++)
+						alleles[i] = Sequence.revCompSeq(alleles[i]);	
+				}
+				
+				bw_hap.write(seq_sn+"\t"+(pos+1)+"\t"+s[2]+"\t"+alleles[0]+"\t"+altAlleles(alleles));
 				
 				for(int i=0; i<hap_copy; i++) {
 					haps[i].append(hapseq.substring(offset, pos));
 					if(i<alleles.length) {
 						// guarantee one copy of each allele 
-						allele = alleles[i];
+						h = i;
 					} else {
 						// randomly select alleles for the remaining copies of haplotype
-						allele = alleles[Constants.rand.nextInt(alleles.length)];
+						h = Constants.rand.nextInt(alleles.length);
 					}
-					haps[i].append(allele);
+					haps[i].append(alleles[h]);
+					bw_hap.write("\t"+h);
 				}
 				offset = pos+refz;
 			}
 			// append subsequence to the hapseq end
 			for(int i=0; i<hap_copy; i++) haps[i].append(hapseq.substring(offset));
 			br_vcf.close();
+			bw_hap.close();
 			
 			// now write output files
 			for(int i=0; i<hap_copy; i++) {
 				BufferedWriter bw = Utils.getBufferedWriter(out_f+".hap."+i+".fasta");
-				bw.write(Sequence.formatOutput(hapseqs.get(0).seq_sn()+"_HAP"+i, 
+				bw.write(Sequence.formatOutput(seq_sn+"_HAP"+i, 
 						haps[i].toString()));
 				bw.close();
 			}
@@ -129,5 +146,15 @@ public class HaplotypeSimulator extends Executor {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private String altAlleles(String[] alleles) {
+		// TODO Auto-generated method stub
+		StringBuilder str = new StringBuilder(alleles[1]);
+		for(int i=2; i<alleles.length; i++) {
+			str.append(",");
+			str.append(alleles[i]);
+		}
+		return str.toString();
 	}
 }
